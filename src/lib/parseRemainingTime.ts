3cf6d1efc7ce9a671e512c50c7c85577b5c7185a
Ryan Bradley <ryanbradley315@gmail.com>
2023-11-09 19:07:12 -0600

    Refactor time parsing function to ensure minutes and seconds are converted to strings prior to return statement.

--- src/lib/parseRemainingTime.ts
+++ src/lib/parseRemainingTime.ts
@@ -2,8 +2,8 @@ export const parseRemainingTime = (remainingtime: number) => {
   const minutes = Math.floor(remainingtime / 60)
   const seconds = remainingtime % 60
 
-  const minutesString = minutes < 10 ? '0' + minutes : minutes
-  const secondsString = seconds < 10 ? '0' + seconds : seconds
+  const minutesString = minutes < 10 ? '0' + minutes : minutes.toString()
+  const secondsString = seconds < 10 ? '0' + seconds : seconds.toString()
 
-  return minutesString + ':' + secondsString
+  return `${minutesString}:${secondsString}`
 }
